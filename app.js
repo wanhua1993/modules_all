8abb9af8c04bf9978abc373f7c88923b54cc7650
wanhua1993 <1126923673@qq.com>
2018-05-30 11:20:35 +0800

    122

--- app.js
+++ app.js
@@ -4,22 +4,23 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var session = require('express-session');
+var log4js = require('log4js');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
 
-// view engine setup
+// 设置魔板引擎
 app.set('view engine', 'html');
 app.set('views', path.join(__dirname, 'views'));
 app.engine('.html', require('ejs').__express);
-
+// 静态资源 post请求
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+// 解决跨域 问题
 app.all('*', function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
@@ -29,11 +30,17 @@ app.all('*', function (req, res, next) {
   res.header("Content-Type", "text/html");
   next();
 });
+// session 模块
 app.use(session({
   secret: 'sessiontest',//与cookieParser中的一致
   resave: true,
-  saveUninitialized:true
+  saveUninitialized: true
 }));
+// 日志模块
+log4js.configure('./config/log4js.json');
+var logger = log4js.getLogger('log_file');
+app.use(log4js.connectLogger(logger, { level: 'auto', format: ':method :url :date :status' }));
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
